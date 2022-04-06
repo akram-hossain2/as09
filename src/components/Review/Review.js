f075c0db8915a096d5baf1e68f76586d29c43db4
akram-hossain2 <akram-hossain2>
2022-04-06 13:59:50 +0600

    revew section

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -4,7 +4,15 @@ import React from 'react';
 const Review = ({review}) => {
 
     return (
+        <div className='card-box'>
+        <div className='card-text'>
         
+            <h1>{review.name}</h1>
+        </div>
+           <p>{review.review}</p>
+           <h2>Rating : {review.Rating}</h2>
+        </div>
+
 
     );
 };
